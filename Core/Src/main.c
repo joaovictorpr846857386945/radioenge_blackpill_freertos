87390c2875af4d332ab9ab9b046a49d0ad08af1d
Joao Victor <joaovictorpr@gmail.com>
2024-03-04 15:34:15 -0300

    teste

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,5 +1,5 @@
 /* USER CODE BEGIN Header */
-/** asdhasdiandiowndaiwnd
+/**
   ******************************************************************************
   * @file           : main.c
   * @brief          : Main program body
